916179728f34603d5de88c9bb21714d3b735b164
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-09-28 00:14:46 -0300

    Business and Database created

---
+++ Database/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class Client
+    {
+        public string name { get; set; }
+        public List<Client> Clients()
+        {
+            var clients = new List<Client>();
+            clients.Add(new Client() { name = "Gustavo" });
+            return clients;
+        }
+    }
+}
